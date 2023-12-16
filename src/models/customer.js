5f6418fd5a03771f11c9b20dc89ac1be148b3bc8
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-12-16 07:17:03 +0700

    create schema customer

---
+++ src/models/customer.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// Set up database
+const customerSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: String,
+        city: String,
+        adress: String,
+        phone: String,
+        image: String,
+        description: String,
+    },
+    { timestamps: true }
+);
+
+const Customer = mongoose.model('Customer', customerSchema);
+
+module.exports = Customer;
